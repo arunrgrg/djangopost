47220ba0654d4adbe659bb1d8581425cf44dcb79
arunrgrg <arunrgrg@gmail.com>
2022-02-03 12:22:33 +0530

    firstcommit

---
+++ apitest/models.py
@@ -0,0 +1,30 @@
+from unicodedata import category
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Post(models.Model):
+
+    category = (
+        ('couple jokes','couple jokes'),
+        ('politics','politics'),
+        ('science', 'science'),
+        ('movies','movies'),
+        ('health', 'health'),
+        ('finance','finance'),      
+    )
+
+    post_description = models.CharField(max_length=1000)
+    post_img = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+
+
+class Like(models.Model):
+
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    post = models.ForeignKey(Post)
+  
+
+
+
+
